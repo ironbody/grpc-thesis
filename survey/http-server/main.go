3a99230442c4b596d747e1775e9ab020d35bcf5d
Najem Hamo <najem.hamo@ericsson.com>
2023-03-13 08:45:02 +0100

    HTTP & gRPC Server

--- survey/http-server/main.go
+++ survey/http-server/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, client! You sent: %s", r.URL.Path[1:])
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
