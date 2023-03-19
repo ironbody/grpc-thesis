6aadec06ebbcb5ac82e3006fc61b8b57118d4b38
Simon Saberian <simon.saberian@gmail.com>
2023-03-19 01:13:52 +0100

    moved files in benchmarks folder

---
+++ benchmarks/grpc/client.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	pb "Benchmarks/grpc/tutorial"
+	"context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
+)
+
+const (
+	address     = "localhost:5000"
+	defaultName = "Najem"
+)
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+
+	defer func(conn *grpc.ClientConn) {
+		err := conn.Close()
+		if err != nil {
+			panic("closed")
+		}
+	}(conn)
+
+	c := pb.NewGreeterClient(conn)
+
+	name := defaultName
+
+	r, err := c.SayHello(context.Background(), &pb.HelloRequest{Name: name})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+
+	log.Printf("Greeting: %s", r.Message)
+}
